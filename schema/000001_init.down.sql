ac206dcb176098ed45c2cac427bec1868b9398ef
denis-shcherbinin <denis.shcherbinin1219@gmail.com>
2021-03-29 12:52:10 +0300

    [SCN-50] Доделка БД * Добавлены таблицы отношений: ** user-skills, user-spheres, user-resumes ** resume-skills, resume-spheres ** companie-vacancies ** vacancy-skills, vacancy-spheres ** sphere-skills

--- schema/000001_init.down.sql
+++ schema/000001_init.down.sql
@@ -1,11 +1,20 @@
 DROP TABLE "users_sessions";
+DROP TABLE "users_spheres";
+DROP TABLE "users_skills";
+DROP TABLE "users_resumes";
+DROP TABLE "users";
 
+DROP TABLE "resumes_spheres";
+DROP TABLE "resumes_skills";
 DROP TABLE "resumes";
 
+DROP TABLE "companies_vacancies";
 DROP TABLE "companies";
 
+DROP TABLE "vacancies_spheres";
+DROP TABLE "vacancies_skills";
 DROP TABLE "vacancies";
 
+DROP TABLE "spheres_skills";
 DROP TABLE "skills";
-
-DROP TABLE "users";
+DROP TABLE "spheres";
